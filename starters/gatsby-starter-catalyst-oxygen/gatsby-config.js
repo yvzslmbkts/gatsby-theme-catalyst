2637678733430ce25c368182aba409bd6119290d
Eric Howey <ehowey@gmail.com>
2020-04-30 11:59:28 -0600

    initial sanity setup

--- starters/gatsby-starter-catalyst-oxygen/gatsby-config.js
+++ starters/gatsby-starter-catalyst-oxygen/gatsby-config.js
@@ -1,24 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Oxygen`,
-    description: `Speed up your Gatsby development workflow.`,
-    keywords: [`gatsby`, `theme`, `react`, `blog`],
+    title: `Catalyst Sanity`,
+    description: `Speed up your GatsbyJS development workflow. Designed as a set of opinionated and advanced themes and starters using MDX and Theme-UI. Incorporates gatsby-theme-catalyst-core, gatsby-theme-catalyst-header-top, and gatsby-theme-catalyst-footer.`,
+    keywords: [`gatsby`, `theme`, `react`],
     author: `Eric Howey`,
-    siteUrl: `https://gatsby-starter-catalyst-oxygen.netlify.app`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
+    siteUrl: `https://gatsby-starter-catalyst-sanity.netlify.app`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
     menuLinks: [
       {
         name: `Page 1`,
         link: `/page-1`,
-        type: `internal`, //internal or anchor
-      },
-      {
-        name: `Page 2`,
-        link: `/page-2`,
-        type: `internal`, //internal or anchor
-      },
-      {
-        name: `Blog`,
-        link: `/blog`,
         type: `internal`, //internal or anchor
       },
     ],
@@ -29,20 +19,75 @@ module.exports = {
         location: `footer`, //Options are "all", "header", "footer"
       },
       {
+        name: `Twitter`,
+        link: `https://twitter.com/erchwy`,
+        location: `header`, //Options are "all", "header", "footer"
+      },
+      {
         name: `Github`,
         link: `https://www.github.com/ehowey`,
         location: `all`, //Options are "all", "header", "footer"
-      },
-      {
-        name: `Twitter`,
-        link: `https://www.twitter.com/erchwy`,
-        location: `header`, //Options are "all", "header", "footer"
       },
     ],
   },
   plugins: [
     {
-      resolve: `gatsby-theme-catalyst-oxygen`,
+      resolve: `gatsby-theme-catalyst-core`,
+      options: {
+        // Default options are:
+        // contentPath: `content/pages`,
+        // assetPath: `content/assets`,
+        // displaySiteLogo: true,
+        // displaySiteTitle: true,
+        // displaySiteLogoMobile: true,
+        // displaySiteTitleMobile: true,
+        // invertLogo: false,
+        // useStickyHeader: false,
+        // useSocialLinks: true,
+        // useColorMode: true, // Dark mode is not supported when configuring the theme from SANITY.io dashboard
+        // footerContentLocation: "left", // "left", "right", "center"
+        // remarkImagesWidth: 1440,
+      },
+    },
+    `gatsby-theme-catalyst-header-top`,
+    `gatsby-theme-catalyst-footer`,
+    {
+      resolve: `gatsby-theme-catalyst-sanity`,
+      options: {
+        // Example for an env variable
+        // sanityProjectId: process.env.SANITY_PROJECT_ID,
+        // sanityDataset: process.env.SANITY_DATASET,
+        // sanityToken: process.env.SANITY_TOKEN,
+        //
+        // Default options are:
+        // sanityProjectId: "abc123" // Required
+        // sanityDataset: "production"
+        // sanityToken: null
+        // sanityWatchMode: true
+        // sanityOverlayDrafts: false // Token is required for this
+        // sanityCreatePages: true
+        // sanityCreatePosts: true
+        // sanityCreatePostsList: true
+        // sanityCreateProjects: true
+        // sanityCreateProjectsList: true
+        // sanityPostPath: "/posts"
+        // sanityProjectPath: "/projects"
+        // useSanityTheme: false // Experimental right now
+        sanityProjectId: "5x2tb0v5",
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-catalyst-hydrogen`,
+        short_name: `catalyst`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#cccccc`,
+        display: `minimal-ui`,
+        icon: `content/assets/catalyst-site-icon.png`, // This path is relative to the root of the site.
+      },
     },
   ],
 }
--- themes/gatsby-theme-catalyst-oxygen/gatsby-config.js
+++ themes/gatsby-theme-catalyst-oxygen/gatsby-config.js
@@ -18,9 +18,26 @@ module.exports = (themeOptions) => {
           //footerContentLocation: "left", // "left", "right", "center"
         },
       },
-      `gatsby-theme-catalyst-blog`,
-      `gatsby-theme-catalyst-header-side`,
+      `gatsby-theme-catalyst-header-top`,
       `gatsby-theme-catalyst-footer`,
+      {
+        resolve: `gatsby-theme-catalyst-sanity`,
+        options: {
+          sanityProjectId: themeOptions.sanityProjectId,
+          sanityDataset: themeOptions.sanityDataset,
+          sanityToken: themeOptions.sanityToken,
+          sanityWatchMode: themeOptions.sanityWatchMode,
+          sanityOverlayDrafts: themeOptions.sanityOverlayDrafts,
+          sanityCreatePages: themeOptions.sanityCreatePages,
+          sanityCreatePosts: themeOptions.sanityCreatePosts,
+          sanityCreatePostsList: themeOptions.sanityCreatePostsList,
+          sanityCreateProjects: themeOptions.sanityCreateProjects,
+          sanityCreateProjectsList: themeOptions.sanityCreateProjectsList,
+          pagePath: themeOptions.pagePath,
+          sanityPostPath: themeOptions.sanityPostPath,
+          sanityProjectPath: themeOptions.sanityProjectPath,
+        },
+      },
     ].filter(Boolean),
   }
 }
